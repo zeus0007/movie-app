f8b3fd2b3c4b87b9ad3891e5e02aad4083513f15
zeus <wpdn79@likelion.org>
2019-09-25 10:45:52 +0900

    Setup, JSX & Props

---
+++ test.js
@@ -0,0 +1,8 @@
+const title1 = document.querySelector("#title"); //#title이라는 녀석을 html에서 찾아서 title1에다가 저장한다.
+
+const CLICKED_CLASS = "cliked"; //clicked라는 문자열을 CLICKED_CLASS라는 변수에 저장한다.
+
+function handleClick() {
+  //handleClick이라는 함수를 생성함.
+  const currentClass = title1.className; //위에서 불러온 title1에 저장되어있는(title이라는 아이디의 html요소)들 중 class
+}
